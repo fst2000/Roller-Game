bc26c3d93164e14d4c92c0288f90d06df43c0eef
fst2000 <fst.18.work@gmail.com>
2024-01-04 18:42:54 +0400

    own physics

--- code/scripts/camera_roller.gd
+++ code/scripts/camera_roller.gd
@@ -1,18 +1,29 @@
 extends Camera3D
 
-@export var roller : Roller
-
+@export var roller : Node3D
+@onready var raycast = $raycast
 var camera_lerp = 2
 var distance = 3
 var height = 1
-
 func _process(delta):
 	var roller_velocity = roller.get_velocity()
 	var lerp_direction = lerp(forward(), roller_velocity, delta * camera_lerp)
 	var lerp_axis = lerp(quaternion * Vector3.UP, roller.quaternion * Vector3.UP, delta * camera_lerp)
 	var local_pos = -lerp_direction * distance + lerp_axis * height
+	var global_pos = roller.global_position + local_pos
+	var look_point = roller.global_position + Vector3.UP * height
+	
+	
 	if roller_velocity.length() > 0.01:
-		global_position = roller.global_position + local_pos
+		var pre_check_vector = quaternion * Vector3.FORWARD * 0.3
+		var ray_vector = global_pos - look_point - pre_check_vector
+		raycast.global_position = look_point
+		raycast.target_position = quaternion.inverse() * ray_vector
+		if raycast.is_colliding():
+			var collision_point = raycast.get_collision_point() + pre_check_vector
+			var final_pos = look_point + ray_vector.normalized() * lerp(look_point.distance_to(global_position), look_point.distance_to(collision_point), delta * 20)
+			global_position = final_pos
+		else: global_position = global_pos
 		look_at_direction(lerp_direction, lerp_axis)
 
 func forward() -> Vector3:
---
+++ code/scripts/roller_test.gd
@@ -0,0 +1,53 @@
+extends Node3D
+
+@onready var shape = $shapecast
+@onready var ray = $floor_ray
+var gravity = 10
+var jump_force = 5
+var move_speed = 15
+var rotation_speed = 5
+var side_friction = 5
+var velocity := Vector3.ZERO
+var roller_input := KeyInput.new()
+func get_velocity() -> Vector3:
+	return velocity
+
+func _physics_process(delta):
+	if ray.is_colliding():
+		race(delta)
+	else:
+		fall(delta)
+	
+func move(velocity : Vector3):
+	global_position += velocity 
+
+func look_at_direction(direction : Vector3, axis : Vector3):
+	look_at(global_position + direction, axis)
+
+func forward() -> Vector3:
+	return quaternion * Vector3.FORWARD
+
+func fall(delta):
+	var input = roller_input.input()
+	velocity.y -= gravity * delta
+	move(velocity * delta)
+	rotate(quaternion * Vector3.UP, input.x * delta * rotation_speed)
+
+func race(delta):
+	var input = roller_input.input()
+	var right = quaternion * Vector3.RIGHT
+	var point = ray.get_collision_point()
+	global_position = point
+	var normal = ray.get_collision_normal()
+	var axis_lerp = lerp(quaternion * Vector3.UP, normal, delta * 10)
+	var floor_forward = axis_lerp.cross(right)
+	look_at_direction(floor_forward, axis_lerp)
+	
+	var slope_velocity = (Vector3.DOWN * gravity).slide(normal) * delta
+	velocity = velocity.slide(normal) + slope_velocity
+	var move_velocity = forward().slide(Vector3.UP) * move_speed * -input.z * delta
+	velocity += move_velocity
+	var side_friction_velocity = -right * right.dot(velocity) * delta * side_friction
+	velocity += side_friction_velocity
+	move((velocity) * delta)
+	rotate(quaternion * Vector3.UP, input.x * delta * rotation_speed)
